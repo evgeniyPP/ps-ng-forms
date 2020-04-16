e8f962bd2f2a71303a918d931b9fa8fa8a98b0d8
evgeniyPP <aysanru@gmail.com>
2020-04-16 13:24:07 +0300

    User Settings Form: two-way binding

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.css']
+  styleUrls: ['./user-settings-form.component.css'],
 })
 export class UserSettingsFormComponent implements OnInit {
+  initialUserSettings: IUserSettings = {
+    name: '',
+    emailOffers: false,
+    interfaceStyle: 'light',
+    subscriptionType: 'monthly',
+    notes: '',
+  };
 
-  constructor() { }
+  userSettings: IUserSettings = { ...this.initialUserSettings };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
